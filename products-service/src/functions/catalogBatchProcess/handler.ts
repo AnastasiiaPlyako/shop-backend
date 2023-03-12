593b13acec59f3b093ef7e49759e626aaf176bb6
AnastasiiaPlyako <anastasia_plyako@epam.com>
2023-03-12 11:25:03 +0400

    add unit test

--- products-service/src/functions/catalogBatchProcess/handler.ts
+++ products-service/src/functions/catalogBatchProcess/handler.ts
@@ -1,26 +1,21 @@
-import * as AWS from 'aws-sdk';
+import { SNSClient, PublishCommand } from "@aws-sdk/client-sns";
 
-import { middyfy } from '@libs/lambda';
-import { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
 import { formatInternalError } from '@error/index';
-import schema from './schema';
 
-const catalogBatchProcess: ValidatedEventAPIGatewayProxyEvent <typeof schema> = async (event) => {
+const catalogBatchProcess = async (event) => {
     try {
         if (event?.Records?.length) {
             const data = event?.Records?.map(({ body }) => body);
-            const sns = new AWS.SNS({
-                region: 'eu-west-1'
-            })
+            const sns = new SNSClient({ region: 'eu-west-1' });
             const params = {
                 Message: data.join(', \n'),
                 TopicArn: process.env.SNS_ARN
             };
-            await sns.publish(params).promise()
+            await sns.send(new PublishCommand (params));
         }
     } catch (e) {
         return formatInternalError();
     }
 };
 
-export const main = middyfy(catalogBatchProcess);
+export const main = catalogBatchProcess;
